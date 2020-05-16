5088107d85755359ee61ebd4933fc3e332443794
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-16 23:02:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,11 +22,13 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 display_board(board)
 display_board(board)
 display_board(board)
+
 
 def input_to_index(user_input)
   user_input.to_i - 1
